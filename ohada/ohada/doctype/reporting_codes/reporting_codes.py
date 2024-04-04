acbb3f5df612e81cab0d6b3a05cb7ba8339663e4
dachir <dodziamouzou@gmail.com>
2024-04-04 11:23:32 +0000

    040420241223

---
+++ ohada/ohada/doctype/reporting_codes/reporting_codes.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Richard Amouzou and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class ReportingCodes(Document):
+	pass
